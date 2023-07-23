a1046f42e69f797a2d138f475496eaed2ccb6f87
NicelySpy <79295079+NicelySpy@users.noreply.github.com>
2023-07-23 16:22:44 +0700

    Update genbot.js

--- src/lib/genbot.js
+++ src/lib/genbot.js
@@ -21,35 +21,35 @@ export class GenBot extends WhatsappBot {
     return {
       info(...args) {
         console.log(
-          chalk.bold.bgRgb(51, 204, 51)("INFO "),
+          chalk.bold.bgRgb(51, 204, 51)(" INFO "),
           `[${chalk.rgb(255, 255, 255)(new Date().toUTCString())}]:`,
           chalk.cyan(format(...args))
         );
       },
       error(...args) {
         console.log(
-          chalk.bold.bgRgb(247, 38, 33)("ERROR "),
+          chalk.bold.bgRgb(247, 38, 33)(" ERROR "),
           `[${chalk.rgb(255, 255, 255)(new Date().toUTCString())}]:`,
           chalk.rgb(255, 38, 0)(format(...args))
         );
       },
       warn(...args) {
         console.log(
-          chalk.bold.bgRgb(255, 153, 0)("WARNING "),
+          chalk.bold.bgRgb(255, 153, 0)(" WARNING "),
           `[${chalk.rgb(255, 255, 255)(new Date().toUTCString())}]:`,
           chalk.redBright(format(...args))
         );
       },
       trace(...args) {
         console.log(
-          chalk.grey("TRACE "),
+          chalk.grey(" TRACE "),
           `[${chalk.rgb(255, 255, 255)(new Date().toUTCString())}]:`,
           chalk.white(format(...args))
         );
       },
       debug(...args) {
         console.log(
-          chalk.bold.bgRgb(66, 167, 245)("DEBUG "),
+          chalk.bold.bgRgb(66, 167, 245)(" DEBUG "),
           `[${chalk.rgb(255, 255, 255)(new Date().toUTCString())}]:`,
           chalk.white(format(...args))
         );
@@ -64,9 +64,6 @@ export class GenBot extends WhatsappBot {
       let cmd = file.default || file;
       cmd.name = cmd.name || `${f.split(".")[0]}.${f.split(".")[1]}`;
 
-      //console.log(f, file, cmd)
-      //this.event.set([cmd.name, cmd])
-      //cmd.run = cmd.run.bind(this);
       this.event[cmd.name] = cmd.run;
     }
   }
@@ -79,7 +76,68 @@ export class GenBot extends WhatsappBot {
         let file = await import(`../Plugins/${dir}/${f}`);
         let cmd = file.default || file;
 
-        
+        if('command' in cmd) {
+          let { command } = cmd
+          if('name' in command) {
+            let { name } = command
+            name = name.toString()
+          }
+          if('description' in command) {
+            let { description } = command
+            description = description.toString()
+          }
+          if('aliases' in command) {
+            let { aliases } = command
+            aliases = Array.isArray(aliases) ? aliases : [].push(aliases)
+          }
+          if('category' in command) {
+            let { category } = command
+            category = category.toString()
+          }
+          if('help' in command) {
+            let { help } = command
+            help = Array.isArray(help) ? help : [].push(help)
+          }
+          if('require' in command) {
+            let { require } = command
+            if('diamond' in require) {
+              let { diamond } = require
+              diamond = isNaN(Number(diamond)) ? 0 : Number(diamond)
+            }
+            if('level' in require) {
+              let { level } = require
+              level = isNaN(Number(level)) ? 0 : Number(level)
+            }
+          }
+        }
+        if('expEarning' in cmd) {
+          let { expEarning } = cmd
+          expEarning = isNaN(Number(expEarning)) ? 0 : Number(expEarning)
+        }
+        if('onlyIf' in cmd) {
+          let { onlyIf } = cmd
+          onlyIf = Array.isArray(onlyIf) ? onlyIf : [].push(onlyif)
+        }
+        if('disabled' in cmd) {
+          let { disabled } = cmd
+          disabled = (typeof disabled === 'boolean') ? disabled : true
+        }
+        if('run' in cmd) {
+          let { run } = cmd
+          run = (typeof run === 'function') ? run.bind(this) : (m) => m;
+        }
+        if('runAll' in cmd) {
+          let { runAll } = cmd
+          runAll = (typeof runAll === 'function') ? runAll.bind(this) : (m) => m;
+        }
+        if('runBefore' in cmd) {
+          let { runBefore } = cmd
+          runBefore = (typeof runBefore === 'function') ? runBefore.bind(this) : (m) => m;
+        }
+        if('runAfter' in cmd) {
+          let { runAfter } = cmd
+          runAfter = (typeof runAfter === 'function') ? runAfter.bind(this) : (m) => m;
+        }
         cmd._file = {
           dir,
           file: f,
