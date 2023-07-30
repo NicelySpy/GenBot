0d2c3e1b51542f810b9516d5e5d994827e026f1b
NicelySpy <79295079+NicelySpy@users.noreply.github.com>
2023-07-30 13:12:23 +0700

    Update store.js

--- src/lib/builder/store.js
+++ src/lib/builder/store.js
@@ -1,3 +1,76 @@
+import { readFileSync, writeFileSync, existsSync } from 'fs' 
+  
+ /** 
+  * @type {import('@adiwajshing/baileys')} 
+  */ 
+ import { initAuthCreds, BufferJSON, proto } = from '@whiskeysockets/baileys'
+  
+ /** 
+  * @param {import('@adiwajshing/baileys').WASocket | import('@adiwajshing/baileys').WALegacySocket} 
+  */
+function useSingleFileAuthState(filename = "src/session.json", logger) { 
+     let creds, keys = {}, saveCount = 0 
+     // save the authentication state to a file 
+     const saveState = (forceSave) => { 
+         logger?.trace('saving auth state') 
+         saveCount++ 
+         if (forceSave || saveCount > 5) { 
+             writeFileSync( 
+                 filename, 
+                 // BufferJSON replacer utility saves buffers nicely 
+                 JSON.stringify({ creds, keys }, BufferJSON.replacer, 2) 
+             ) 
+             saveCount = 0 
+         } 
+     } 
+  
+     if (existsSync(filename)) { 
+         const result = JSON.parse( 
+             readFileSync(filename, { encoding: 'utf-8' }), 
+             BufferJSON.reviver 
+         ) 
+         creds = result.creds 
+         keys = result.keys 
+     } else { 
+         creds = initAuthCreds() 
+         keys = {} 
+     } 
+  
+     return { 
+         state: { 
+             creds, 
+             keys: { 
+                 get: (type, ids) => { 
+                     const key = KEY_MAP[type] 
+                     return ids.reduce( 
+                         (dict, id) => { 
+                             let value = keys[key]?.[id] 
+                             if (value) { 
+                                 if (type === 'app-state-sync-key') { 
+                                     value = proto.AppStateSyncKeyData.fromObject(value) 
+                                 } 
+  
+                                 dict[id] = value 
+                             } 
+  
+                             return dict 
+                         }, {} 
+                     ) 
+                 }, 
+                 set: (data) => { 
+                     for (const _key in data) { 
+                         const key = KEY_MAP[_key] 
+                         keys[key] = keys[key] || {} 
+                         Object.assign(keys[key], data[_key]) 
+                     } 
+  
+                     saveState() 
+                 } 
+             } 
+         }, 
+         saveState 
+     } 
+                                   }
 function bind(conn) {
   if (!conn.chats) conn.chats = {};
   /**
