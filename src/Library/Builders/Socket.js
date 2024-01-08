4f46ffb51c8c19343be55842f0a863e723cec5bf
NicelySpy <79295079+NicelySpy@users.noreply.github.com>
2024-01-08 18:47:23 +0700

    Update Socket.js

--- src/Library/Builders/Socket.js
+++ src/Library/Builders/Socket.js
@@ -142,6 +142,386 @@ class Socket {
       return m;
     }
   }
+  
+  async pushMessage(m) {
+    if (!m) return;
+    if (!Array.isArray(m)) m = [m];
+    for (const message of m) {
+      try {
+        // if (!(message instanceof proto.WebMessageInfo)) continue // https://github.com/adiwajshing/Baileys/pull/696/commits/6a2cb5a4139d8eb0a75c4c4ea7ed52adc0aec20f
+        if (!message) continue;
+        if (
+          message.messageStubType &&
+          message.messageStubType != WAMessageStubType.CIPHERTEXT
+        )
+          this.processMessageStubType(message).catch(console.error);
+        const _mtype = Object.keys(message.message || {});
+        const mtype =
+          (!["senderKeyDistributionMessage", "messageContextInfo"].includes(
+            _mtype[0]
+          ) &&
+            _mtype[0]) ||
+          (_mtype.length >= 3 &&
+            _mtype[1] !== "messageContextInfo" &&
+            _mtype[1]) ||
+          _mtype[_mtype.length - 1];
+        let chat = this.decodeJid(
+          message.key.remoteJid ||
+            message.message?.senderKeyDistributionMessage?.groupId ||
+            ""
+        );
+        if (message.message?.[mtype]?.contextInfo?.quotedMessage) {
+          let context = message.message[mtype].contextInfo;
+          let participant = this.decodeJid(context.participant);
+          let remoteJid = this.decodeJid(context.remoteJid || participant);
+
+          let quoted = message.message[mtype].contextInfo.quotedMessage;
+          if (remoteJid && remoteJid !== "status@broadcast" && quoted) {
+            let qMtype = Object.keys(quoted)[0];
+            if (qMtype == "conversation") {
+              quoted.extendedTextMessage = { text: quoted[qMtype] };
+              delete quoted.conversation;
+              qMtype = "extendedTextMessage";
+            }
+            if (!quoted[qMtype].contextInfo) quoted[qMtype].contextInfo = {};
+            quoted[qMtype].contextInfo.mentionedJid =
+              context.mentionedJid ||
+              quoted[qMtype].contextInfo.mentionedJid ||
+              [];
+            const isGroup = remoteJid.endsWith("g.us");
+            if (isGroup && !participant) participant = remoteJid;
+            const qM = {
+              key: {
+                remoteJid,
+                fromMe: areJidsSameUser(this.user.jid, remoteJid),
+                id: context.stanzaId,
+                participant,
+              },
+              message: JSON.parse(JSON.stringify(quoted)),
+              ...(isGroup ? { participant } : {}),
+            };
+            let qChats = this.chats[participant];
+            if (!qChats)
+              qChats = this.chats[participant] = {
+                id: participant,
+                isChats: !isGroup,
+              };
+            if (!qChats.messages) qChats.messages = {};
+            if (!qChats.messages[context.stanzaId] && !qM.key.fromMe)
+              qChats.messages[context.stanzaId] = qM;
+            let qChatsMessages;
+            if ((qChatsMessages = Object.entries(qChats.messages)).length > 40)
+              qChats.messages = Object.fromEntries(
+                qChatsMessages.slice(30, qChatsMessages.length)
+              ); // maybe avoid memory leak
+          }
+        }
+        if (!chat || chat === "status@broadcast") continue;
+        const isGroup = chat.endsWith("@g.us");
+        let chats = this.chats[chat];
+        if (!chats) {
+          if (isGroup) await this.insertAllGroup().catch(console.error);
+          chats = this.chats[chat] = {
+            id: chat,
+            isChats: true,
+            ...(this.chats[chat] || {}),
+          };
+        }
+        let metadata, sender;
+        if (isGroup) {
+          if (!chats.subject || !chats.metadata) {
+            metadata =
+              (await this.groupMetadata(chat).catch((_) => ({}))) || {};
+            if (!chats.subject) chats.subject = metadata.subject || "";
+            if (!chats.metadata) chats.metadata = metadata;
+          }
+          sender = this.decodeJid(
+            (message.key?.fromMe && this.user.id) ||
+              message.participant ||
+              message.key?.participant ||
+              chat ||
+              ""
+          );
+          if (sender !== chat) {
+            let chats = this.chats[sender];
+            if (!chats) chats = this.chats[sender] = { id: sender };
+            if (!chats.name) chats.name = message.pushName || chats.name || "";
+          }
+        } else if (!chats.name)
+          chats.name = message.pushName || chats.name || "";
+        if (
+          ["senderKeyDistributionMessage", "messageContextInfo"].includes(mtype)
+        )
+          continue;
+        chats.isChats = true;
+        if (!chats.messages) chats.messages = {};
+        const fromMe =
+          message.key.fromMe || areJidsSameUser(sender || chat, this.user.id);
+        if (
+          !["protocolMessage"].includes(mtype) &&
+          !fromMe &&
+          message.messageStubType != WAMessageStubType.CIPHERTEXT &&
+          message.message
+        ) {
+          delete message.message.messageContextInfo;
+          delete message.message.senderKeyDistributionMessage;
+          chats.messages[message.key.id] = JSON.parse(
+            JSON.stringify(message, null, 2)
+          );
+          let chatsMessages;
+          if ((chatsMessages = Object.entries(chats.messages)).length > 40)
+            chats.messages = Object.fromEntries(
+              chatsMessages.slice(30, chatsMessages.length)
+            );
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
+  async sendContact(jid, data, quoted, options) {
+    if (!Array.isArray(data[0]) && typeof data[0] === "string") data = [data];
+    let contacts = [];
+    for (let [number, name] of data) {
+      number = number.replace(/[^0-9]/g, "");
+      let njid = number + "@s.whatsapp.net";
+      let biz =
+        (await this.getBusinessProfile(njid).catch((_) => null)) || {};
+      let vcard = `  
+BEGIN:VCARD  
+VERSION:3.0  
+N:;${name.replace(/\n/g, "\\n")};;;  
+FN:${name.replace(/\n/g, "\\n")}  
+TEL;type=CELL;type=VOICE;waid=${number}:${PhoneNumber("+" + number).getNumber(
+        "international"
+      )}${
+        biz.description
+          ? `
+X-WA-BIZ-NAME:${(
+    this.chats[njid]?.vname ||
+    this.getName(njid) ||
+    name
+  ).replace(/\n/, "\\n")}
+X-WA-BIZ-DESCRIPTION:${biz.description.replace(/\n/g, "\\n")}  
+  `.trim()
+          : ""
+      } 
+END:VCARD  
+          `.trim();
+      contacts.push({ vcard, displayName: name });
+    }
+    return await this.sendMessage(
+      jid,
+      {
+        ...options,
+        contacts: {
+          ...options,
+          displayName:
+            (contacts.length >= 2
+              ? `${contacts.length} kontak`
+              : contacts[0].displayName) || null,
+          contacts,
+        },
+      },
+      { quoted, ...options }
+    );
+  }
+  
+  async processMessageStubType(m) {
+    if (!m.messageStubType) return;
+    const chat = this.decodeJid(
+      m.key?.remoteJid || m.message?.senderKeyDistributionMessage?.groupId || ""
+    );
+    if (!chat || chat === "status@broadcast") return;
+    const emitGroupUpdate = (update) => {
+      this.ev.emit("groups.update", [{ id: chat, ...update }]);
+    };
+    switch (m.messageStubType) {
+      case WAMessageStubType.REVOKE:
+      case WAMessageStubType.GROUP_CHANGE_INVITE_LINK:
+        emitGroupUpdate({ revoke: m.messageStubParameters[0] });
+        break;
+      case WAMessageStubType.GROUP_CHANGE_ICON:
+        emitGroupUpdate({ icon: m.messageStubParameters[0] });
+        break;
+      default: {
+        console.log({
+          messageStubType: m.messageStubType,
+          messageStubParameters: m.messageStubParameters,
+          type: WAMessageStubType[m.messageStubType],
+        });
+        break;
+      }
+    }
+    const isGroup = chat?.endsWith("@g.us");
+    if (!isGroup) return;
+    let chats = this.chats[chat];
+    if (!chats) chats = this.chats[chat] = { id: chat };
+    chats.isChats = true;
+    const metadata = await this.groupMetadata(chat).catch((_) => null);
+    if (!metadata) return;
+    chats.subject = metadata.subject;
+    chats.metadata = metadata;
+  }
+  async getName(jid = "", withoutContact = false) {
+    jid = this.decodeJid(jid);
+    withoutContact = this.withoutContact || withoutContact;
+    let v;
+    if (jid.endsWith("@g.us"))
+      return new Promise(async (resolve) => {
+        v = this.chats[jid] || {};
+        if (!(v.name || v.subject)) v = (await this.groupMetadata(jid)) || {};
+        resolve(
+          v.name ||
+            v.subject ||
+            PhoneNumber("+" + jid.replace("@s.whatsapp.net", "")).getNumber(
+              "international"
+            )
+        );
+      });
+    else
+      v =
+        jid === "0@s.whatsapp.net"
+          ? {
+              jid,
+              vname: "WhatsApp",
+            }
+          : areJidsSameUser(jid, this.user.id)
+          ? this.user
+          : this.chats[jid] || {};
+    return (
+      (withoutContact ? "" : v.name) ||
+      v.subject ||
+      v.vname ||
+      v.notify ||
+      v.verifiedName ||
+      PhoneNumber("+" + jid.replace("@s.whatsapp.net", "")).getNumber(
+        "international"
+      )
+    );
+  }
+  async insertAllGroup() {
+    const groups =
+      (await this.groupFetchAllParticipating().catch((_) => null)) || {};
+    for (const group in groups)
+      this.chats[group] = {
+        ...(this.chats[group] || {}),
+        id: group,
+        subject: groups[group].subject,
+        isChats: true,
+        metadata: groups[group],
+      };
+    return this.chats;
+  }
+    async cMod(jid, message, text = "", sender = this.user.jid, options = {}) {
+    if (options.mentions && !Array.isArray(options.mentions))
+      options.mentions = [options.mentions];
+    let copy = message.toJSON();
+    delete copy.message.messageContextInfo;
+    delete copy.message.senderKeyDistributionMessage;
+    let mtype = Object.keys(copy.message)[0];
+    let msg = copy.message;
+    let content = msg[mtype];
+    if (typeof content === "string") msg[mtype] = text || content;
+    else if (content.caption) content.caption = text || content.caption;
+    else if (content.text) content.text = text || content.text;
+    if (typeof content !== "string") {
+      msg[mtype] = { ...content, ...options };
+      msg[mtype].contextInfo = {
+        ...(content.contextInfo || {}),
+        mentionedJid:
+          options.mentions || content.contextInfo?.mentionedJid || [],
+      };
+    }
+    if (copy.participant)
+      sender = copy.participant = sender || copy.participant;
+    else if (copy.key.participant)
+      sender = copy.key.participant = sender || copy.key.participant;
+    if (copy.key.remoteJid.includes("@s.whatsapp.net"))
+      sender = sender || copy.key.remoteJid;
+    else if (copy.key.remoteJid.includes("@broadcast"))
+      sender = sender || copy.key.remoteJid;
+    copy.key.remoteJid = jid;
+    copy.key.fromMe = areJidsSameUser(sender, this.user.id) || false;
+    return proto.WebMessageInfo.fromObject(copy);
+    }
+    async downloadM(m, type, saveToFile) {
+    let filename;
+    if (!m || !(m.url || m.directPath)) return Buffer.alloc(0);
+    const stream = await downloadContentFromMessage(m, type);
+    let buffer = Buffer.from([]);
+    for await (const chunk of stream) {
+      buffer = Buffer.concat([buffer, chunk]);
+    }
+    if (saveToFile) ({ filename } = await this.getFile(buffer, true));
+    return saveToFile && fs.existsSync(filename) ? filename : buffer;
+    }
+  async updateProfileStatus(status) {
+    return this.query({
+      tag: "iq",
+      attrs: {
+        to: "s.whatsapp.net",
+        type: "set",
+        xmlns: "status",
+      },
+      content: [
+        {
+          tag: "status",
+          attrs: {},
+          content: Buffer.from(status, "utf-8"),
+        },
+      ],
+    });
+    }
+  async parseMention(text = "") {
+    return [...text.matchAll(/@([0-9]{5,16}|0)/g)].map(
+      (v) => v[1] + "@s.whatsapp.net"
+    );
+  }
+  async copyNforward(jid, message, forwardingScore = true, options = {}) {
+    let vtype;
+    if (options.readViewOnce && message.message.viewOnceMessage?.message) {
+      vtype = Object.keys(message.message.viewOnceMessage.message)[0];
+      delete message.message.viewOnceMessage.message[vtype].viewOnce;
+      message.message = proto.Message.fromObject(
+        JSON.parse(JSON.stringify(message.message.viewOnceMessage.message))
+      );
+      message.message[vtype].contextInfo =
+        message.message.viewOnceMessage.contextInfo;
+    }
+    let mtype = Object.keys(message.message)[0];
+    let m = generateForwardMessageContent(message, !!forwardingScore);
+    let ctype = Object.keys(m)[0];
+    if (
+      forwardingScore &&
+      typeof forwardingScore === "number" &&
+      forwardingScore > 1
+    )
+      m[ctype].contextInfo.forwardingScore += forwardingScore;
+    m[ctype].contextInfo = {
+      ...(message.message[mtype].contextInfo || {}),
+      ...(m[ctype].contextInfo || {}),
+    };
+    m = generateWAMessageFromContent(jid, m, {
+      ...options,
+      userJid: this.user.jid,
+    });
+    await this.relayMessage(jid, m.message, {
+      messageId: m.key.id,
+      additionalAttributes: { ...options },
+    });
+    return m;
+    }
+  loadMessage(messageID) {
+    return Object.entries(this.chats)
+      .filter(([_, { messages }]) => typeof messages === "object")
+      .find(([_, { messages }]) =>
+        Object.entries(messages).find(
+          ([k, v]) => k === messageID || v.key?.id === messageID
+        )
+      )?.[1].messages?.[messageID];
+  }
+  
   reply(jid, text = "", quoted, options) {
     return Buffer.isBuffer(text)
       ? this.sendFile(jid, text, "file", "", quoted, false, options) 
@@ -165,4 +545,5 @@ class Socket {
       ...options,
     });
   }
+  
   
